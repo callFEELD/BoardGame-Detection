edaa105c7f928713d33f68b72343a66cc8c89ad6
callFEELD <callfeeld@gmail.com>
2020-01-20 22:59:18 +0100

    Added cv chessboardFinder. Added estimation for next corners in the chessboard. Added  method to calculate the intersections. Added partly self written chessboard finder.

--- Example_board_detection.py
+++ Example_board_detection.py
@@ -13,36 +13,72 @@ import cv2
 import numpy as np
 import imutils
 
-#fname = "data/20191113_134305.jpg"
-#fname = "data/20191113_134320.jpg" # top down view
-fname = "data/20191113_134334.jpg" 
+fname = "data/20191113_134305.jpg"
+fname = "data/20191113_134320.jpg" # top down view
+fname = "data/20191113_134334.jpg"
+fname = "data/20191113_134324.jpg"
+#fname = "data/20191113_134307.jpg"
+
 
 # image loading and preperation
 image = cv2.imread(fname)
 image = cv2.resize(image,(int(4032/6), int(2268/6)))
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 50, 200)
 
-# detecting features
-corners = bd.get_corners(gray)
-lines = bd.get_lines(edges)
+# finding chessboard
+corners = bd.find_chessboard(gray, (7,7))
+if corners is not None:
+    cv2.drawChessboardCorners(image, (7,7), corners, 1)
 
-# adding the lines to the image
-if lines is not None:
-    # draw lines
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
+    # estimating the next corners that are missing (due to a 9x9 board)
+    new_corner = bd.estimate_next_corner(corners[0:2])
+    front_red = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
 
-# add the corners to the image
-for corner in corners:
-    x,y = corner.ravel()
-    cv2.circle(image,(x,y),3,255,-1)
+    new_corner = bd.estimate_next_corner([corners[-1], corners[-2]])
+    front_pink = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+
+    new_corner = bd.estimate_next_corner([corners[0], corners[7]])
+    red_orange1 = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    new_corner = bd.estimate_next_corner([corners[-1], corners[-8]])
+    pink_blue1 = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    new_corner = bd.estimate_next_corner([corners[6], corners[5]])
+    back_red = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    new_corner = bd.estimate_next_corner([corners[6], corners[13]])
+    red_orange2 = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    new_corner = bd.estimate_next_corner([corners[-7], corners[-14]])
+    pink_blue2 = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    new_corner = bd.estimate_next_corner([corners[-7], corners[-6]])
+    back_pink = new_corner
+    cv2.circle(image,(new_corner[0],new_corner[1]),3,255,-1)
+
+    # build lines from the corners and get the intersection
+    intersect1 = bd.get_intersect(front_red, back_pink, red_orange1, red_orange2)
+    cv2.circle(image,(int(intersect1[0]),int(intersect1[1])),3,(255,255,255),-1)
+
+    intersect2 = bd.get_intersect(front_red, back_pink, pink_blue1, pink_blue2)
+    cv2.circle(image,(int(intersect2[0]),int(intersect2[1])),3,(255,255,255),-1)
+
+    intersect3 = bd.get_intersect(back_red, front_pink, red_orange1, red_orange2)
+    cv2.circle(image,(int(intersect3[0]),int(intersect3[1])),3,(255,255,255),-1)
+
+    intersect4 = bd.get_intersect(back_red, front_pink, pink_blue1, pink_blue2)
+    cv2.circle(image,(int(intersect4[0]),int(intersect4[1])),3,(255,255,255),-1)
 
 # Show results
 cv2.imshow('Corners and Lines',image)
-cv2.imshow('Edges',edges)
-cv2.imshow('Gray',gray)
 
 # De-allocate any associated memory usage   
 if cv2.waitKey(0) & 0xff == 27:  
--- src/board_detection/board_detection.py
+++ src/board_detection/board_detection.py
@@ -13,22 +13,125 @@ import cv2              # for general image processing
 import numpy as np      # for math operations
 
 
+def find_chessboard(image, chessboard_size):
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    ret, corners = cv2.findChessboardCorners(image, chessboard_size)
+    #    If found, add object points, image points (after refining them)
+    if ret == True:
+        cv2.cornerSubPix(image, corners, (11,11), (-1,-1), criteria)
+        return corners
+    return None
+
+def estimate_next_corner(corners):
+    # first get the first corner
+    corner1 = corners[0][0]
+    x1 = corner1[0]
+    y1 = corner1[1]
+
+    # get the corner before 
+    corner2 = corners[1][0]
+    x2 = corner2[0]
+    y2 = corner2[1]
+    
+    y_offset = y2 - y1
+    x_offset = x2 - x1
+    
+    return [x1-x_offset, y1-y_offset]
+
+def get_intersect(a1, a2, b1, b2):
+    """ 
+    Returns the point of intersection of the lines passing through a2,a1 and b2,b1.
+    a1: [x, y] a point on the first line
+    a2: [x, y] another point on the first line
+    b1: [x, y] a point on the second line
+    b2: [x, y] another point on the second line
+    """
+    s = np.vstack([a1,a2,b1,b2])        # s for stacked
+    h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+    l1 = np.cross(h[0], h[1])           # get first line
+    l2 = np.cross(h[2], h[3])           # get second line
+    x, y, z = np.cross(l1, l2)          # point of intersection
+    if z == 0:                          # lines are parallel
+        return (float('inf'), float('inf'))
+    return (x/z, y/z)
+
+# For self written chessboard finder
+
 # detection of coners 
 def get_corners(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     MAX_CORNERS:int = 1000
     QUALITY_LEVEL:float = 0.001
-    MIN_DISTANCE:int = 10
-    return cv2.goodFeaturesToTrack(image, MAX_CORNERS, QUALITY_LEVEL, MIN_DISTANCE, useHarrisDetector=True)
-
+    MIN_DISTANCE:int = 15
+    return cv2.goodFeaturesToTrack(gray, MAX_CORNERS, QUALITY_LEVEL, MIN_DISTANCE, useHarrisDetector=True)
 
 # getting lines based on Hough Lines
 def get_lines(image):
     rho = 1  # distance resolution in pixels of the Hough grid
     theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    threshold = 80  # minimum number of votes (intersections in Hough grid cell)
+    threshold = 75  # minimum number of votes (intersections in Hough grid cell)
     min_line_length = 100  # minimum number of pixels making up a line
     max_line_gap = 100000  # maximum gap in pixels between connectable line segments
     lines = cv2.HoughLinesP(image, rho, theta, threshold, np.array([]),
                         min_line_length, max_line_gap)
     
     return lines
+
+def get_intersections(lines):
+    ret_intersections = []
+    if lines is not None:
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                for nline in lines:
+                    for nx1,ny1,nx2,ny2 in nline:
+                        px, py = get_intersect([x1, y1], [x2, y2], [nx1, ny1], [nx2, ny2])
+
+                        if px != float('inf') and py != float('inf'):
+                            ret_intersections.append([px, py])
+    
+    return ret_intersections
+
+def is_close_to(point1, point2):
+    diff = 4 # 4pixels
+
+    p1x, p1y = point1
+    p2x, p2y = point2
+
+    if abs(p1x - p2x) <= diff and abs(p1y - p2y) <= diff:
+        return True
+    return False
+
+def find_lines_with_corners(img, lines, corners):
+    max_diff = 2 # 4 pixels
+    """
+    # get the x positions of all corners
+    x_corners = [corner.ravel()[0] for corner in corners]
+    y_corners = [corner.ravel()[0] for corner in corners]
+
+    # get possible y's of the y corners based on the linera function of the line
+    possible_ys = np.interp(x_corners, [x1, x2], [y1, y2])
+    """
+
+    ret_lines = []
+    if lines is not None:
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+
+                xp = [x1, x2]
+                fp = [y1, y2]
+
+                amount = 0
+                for corner in corners:
+                    x,y = corner
+
+                    # check if corner is on the line
+                    ty = np.interp(x, xp, fp)
+
+                    if abs(int(ty)-y) <= max_diff:
+                        cv2.circle(img,(x,int(ty)),1,(255,255,0),-1)
+                        amount += 1
+
+                if amount >= 5: #and amount <= 12:
+                    ret_lines.append(line)
+    return ret_lines
