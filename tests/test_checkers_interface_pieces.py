48117dd0ba92748a35d5ebe686cf6f65411e7739
callFEELD <callfeeld@gmail.com>
2020-02-16 20:55:00 +0100

    added test for checkers pieces interface

---
+++ tests/test_checkers_interface_pieces.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import unittest
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
+
+from src.checkers_interface import Piece,\
+    InvalidPlayer, InvalidPiecePosition
+
+
+class TestPieceInterface(unittest.TestCase):
+
+    def test_set_correct_position(self):
+        position = 5
+        piece = Piece()
+        piece.set_position(position)
+
+        self.assertEqual(piece.get_position(), position)
+
+    def test_set_wrong_position1(self):
+        position = 0
+        piece = Piece()
+        self.assertRaises(InvalidPiecePosition, piece.set_position, position)
+
+    def test_set_wrong_position2(self):
+        position = 33
+        piece = Piece()
+        self.assertRaises(InvalidPiecePosition, piece.set_position, position)
+
+    def test_set_correct_player(self):
+        player = 2
+        piece = Piece()
+        piece.set_player(player)
+
+        self.assertEqual(piece.get_player(), player)
+
+    def test_set_wrong_player1(self):
+        player = 0
+        piece = Piece()
+        self.assertRaises(InvalidPlayer, piece.set_player, player)
+
+    def test_set_wrong_player2(self):
+        player = 3
+        piece = Piece()
+        self.assertRaises(InvalidPlayer, piece.set_player, player)
+
+    def test_set_king(self):
+        king = True
+        piece = Piece()
+        piece.set_king(king)
+        self.assertEqual(piece.is_king(), king)
+
+    def test_set_everything_correct(self):
+        position = 25
+        player = 2
+        king = True
+        piece = Piece(position=position, player=player, king=king)
+
+        self.assertEqual(piece.get_player(), player)
+        self.assertEqual(piece.get_position(), position)
+        self.assertEqual(piece.is_king(), king)
+
+
+if __name__ == "__main__":
+    unittest.main()
