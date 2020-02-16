cadbef2707a1445f058903c0a684f089400a5962
callFEELD <callfeeld@gmail.com>
2020-02-16 20:56:25 +0100

    Updated Checkersboard classes

--- src/CheckersBoard.py
+++ src/CheckersBoard.py
@@ -24,48 +24,5 @@ class CheckerBoardSquare:
     enum of checkr board square colors
     """
     BLACK_SQUARE = 1
-    WHITE_SQUARE = 0
-
-
-class CheckersBoard:
-    """
-    Example of a 4x4 Checkers Board
-    |------------|
-    | 0  1  0  1 | <- Black Figure start row
-    | 0  0  0  0 |
-    | 0  0  0  0 |
-    |-1  0 -1  0 | <- White Figure start row
-    |------------|
-    """
-    # Board size --> SIZExSIZE Board
-    SIZE = 8
-
-    # The board itself
-    board = []
-
-    def __init__(self):
-        self._create_empty_board()
-
-    def _create_empty_board(self):
-        """
-        This method creates the empty state of the board, without figures
-        """
-        self.board = []
-        for _ in range(self.SIZE):
-            row = []
-            for _ in range(self.SIZE):
-                row.append(CheckersState.EMPTY)
-            self.board.append(row)
-
-    def reset(self):
-        """
-        This resets the board to the beginning (empty) state with no figures
-        """
-        self.board = []
-        self._create_empty_board()
-
-    def get(self):
-        """
-        This method returns the board
-        """
-        return self.board
+    WHITE_SQUARE = -1
+    UNDEFINED = 0
