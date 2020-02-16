1ba187e5869a4cc31e9046be217ccc79d8e5d7ad
callFEELD <callfeeld@gmail.com>
2020-02-16 20:53:20 +0100

    added parameter for square color threshold

--- config.py
+++ config.py
@@ -7,9 +7,13 @@ import numpy as np
 """Board Square Colors
 """
 # White square colors
-HSV_WHITE_SQUARE_LOWER: np.array(size=(1, 3)) = np.array([0, 0, 200])
-HSV_WHITE_SQUARE_UPPER: np.array(size=(1, 3)) = np.array([180, 255, 255])
+HSV_WHITE_SQUARE_LOWER: np.array = np.array([0, 0, 200])
+HSV_WHITE_SQUARE_UPPER: np.array = np.array([180, 255, 255])
 
 # black square colors
-HSV_BLACK_SQUARE_LOWER: np.array(size=(1, 3)) = np.array([0, 0, 0])
-HSV_BLACK_SQUARE_UPPER: np.array(size=(1, 3)) = np.array([255, 255, 190])
+HSV_BLACK_SQUARE_LOWER: np.array = np.array([0, 0, 0])
+HSV_BLACK_SQUARE_UPPER: np.array = np.array([255, 255, 190])
+
+# Square color treshold
+# At what percentage a square should be defined as black or white
+SQUARE_COLOR_THRESHOLD: float = 0.5
