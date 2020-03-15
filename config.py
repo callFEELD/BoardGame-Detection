7874c542aedbb9adbcfd209612dbe39a56b531d6
callFEELD <callfeeld@gmail.com>
2020-03-15 23:14:35 +0100

    Added HSV Figure colors

--- config.py
+++ config.py
@@ -16,6 +16,14 @@ HSV_WHITE_SQUARE_UPPER: np.array = np.array([180, 255, 255])
 HSV_BLACK_SQUARE_LOWER: np.array = np.array([0, 0, 0])
 HSV_BLACK_SQUARE_UPPER: np.array = np.array([255, 255, 190])
 
+# black figure colors
+HSV_BLACK_FIGURE_LOWER = np.array([-30, 140, 0])
+HSV_BLACK_FIGURE_UPPER = np.array([30, 255, 160])
+
+# white figure colors
+HSV_WHITE_FIGURE_LOWER = np.array([12, 60, 150])
+HSV_WHITE_FIGURE_UPPER = np.array([60, 255, 255])
+
 # Square color treshold
 # At what percentage a square should be defined as black or white
 SQUARE_COLOR_THRESHOLD: float = 0.5
