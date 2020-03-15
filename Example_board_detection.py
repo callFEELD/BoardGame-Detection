00bb1757be503e08ec0a9397bfc14781677d9f49
callFEELD <callfeeld@gmail.com>
2020-03-15 23:16:11 +0100

    added mouse interaction to find the 4 board corners

--- Example_board_detection.py
+++ Example_board_detection.py
@@ -10,15 +10,22 @@ Example Board Detection
 # Imports
 from src.board_detection import board_detection as bd
 from config import DEBUG
+from src.algo import grid_detection as gd
+
+from src import ColorDetector
+from config import HSV_BLACK_FIGURE_LOWER, HSV_BLACK_FIGURE_UPPER, \
+    HSV_WHITE_FIGURE_LOWER, HSV_WHITE_FIGURE_UPPER
 
 import cv2
+import numpy as np
 
+RESIZE_FACTOR = 6
 
-file_name = "data/20191113_134324.jpg"
+file_name = "data/20200228_181947.jpg"
 
 # read the image and make it smaller
 image = cv2.imread(file_name)
-image = cv2.resize(image, (int(4032/4), int(2268/4)))
+image = cv2.resize(image, (int(4032/RESIZE_FACTOR), int(2268/RESIZE_FACTOR)))
 oimage = image.copy()
 
 # finding the chessboard inside an image
@@ -40,15 +47,80 @@ if corners is not None:
     # get the individual squares of the chessboard
     squares = bd.get_chessboard_squares(wrapped, size=(8, 8))
 
+    """
     for pos, square in enumerate(squares):
         if int(pos / 8) == 0:
             print(f"| {square.get_color()}", end='')
         else:
             print(f"| {square.get_color()}", end='')
+    """
+
+    cv2.imshow('Chessboard', wrapped)
+
+
+eckpunkte = []
+
+
+# mouse callback function
+def mouse_callback(event,x,y,flags,param):
+    global eckpunkte
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if len(eckpunkte) < 4:
+            eckpunkte.append([x, y])
+        else:
+            eckpunkte = []
+            eckpunkte.append([x, y])
+        print(x, y)
+        print(eckpunkte)
+
+        if len(eckpunkte) == 4:
+            wrapped = bd.get_chessboard_perspective(cimage, eckpunkte)
+            cv2.imshow('Chessboard', wrapped)
+            board = bd.get_chessboard_squares(wrapped)
+            print(board)
+            cv2.imwrite("wrapped.png", wrapped)
+
+            black = ColorDetector.get_color_mask(wrapped,
+                                                    HSV_BLACK_FIGURE_LOWER,
+                                                    HSV_BLACK_FIGURE_UPPER)
+            white = ColorDetector.get_color_mask(wrapped,
+                                                    HSV_WHITE_FIGURE_LOWER,
+                                                    HSV_WHITE_FIGURE_UPPER)
+            cv2.imshow("black", black)
+            cv2.imshow("white", white)
+
+
+
+
+cimage = image.copy()
+gray = cv2.cvtColor(cimage, cv2.COLOR_BGR2GRAY)
+corners = bd.get_corners(cimage)
+
+
+points = []
+for corner in corners:
+    points.append(corner[0])
+
+for point in points:
+    cv2.circle(image, (point[0], point[1]), 3, (255, 255, 0), -1)
+
+
+NMBR = 72
+cv2.circle(image, (points[NMBR][0], points[NMBR][1]), 3, (255, 255, 255), -1)
+close_points = gd.find_close_points(points[NMBR], points)
+
+distances = gd.get_distance_from_point(points[NMBR], points)
+distances = sorted(distances)
+
+for point in close_points:
+    cv2.circle(image, (point[0], point[1]), 3, 255, -1)
+
 
 # Show results
-cv2.imshow('Corners and Lines', image)
-cv2.imshow('Chessboard', wrapped)
+cv2.imshow('image', image)
+cv2.namedWindow('image',cv2.WINDOW_NORMAL) # Can be resized
+cv2.setMouseCallback('image', mouse_callback) #Mouse callback
+
 
 # De-allocate any associated memory usage
 if cv2.waitKey(0) & 0xff == 27:
