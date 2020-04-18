8fdca18469fbf77fc30562ad657702777ffbbf43
callFEELD <callfeeld@gmail.com>
2020-04-18 21:25:33 +0200

    added a Class which runs the full algorithm

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ HSV_BLACK_SQUARE_UPPER: np.array = np.array([255, 255, 190])
 # black figure colors
 HSV_BLACK_FIGURE_LOWER = np.array([-30, 140, 0])
 HSV_BLACK_FIGURE_UPPER = np.array([30, 255, 160])
+# [153 119  71] [173 139 151]
 
 # white figure colors
 HSV_WHITE_FIGURE_LOWER = np.array([12, 60, 150])
@@ -27,3 +28,55 @@ HSV_WHITE_FIGURE_UPPER = np.array([60, 255, 255])
 # Square color treshold
 # At what percentage a square should be defined as black or white
 SQUARE_COLOR_THRESHOLD: float = 0.5
+
+default_options = {
+    "boarddetector": {
+        "prepare": {
+            "resize": [512, 512]
+        },
+        "corners": {
+            "maxcorners": 500,
+            "qualitylevel": 0.01,
+            "mindistance": 20
+        },
+        "lines": {
+            "harris": {
+                "rho": 1,
+                "theta": 0.01,
+                "threshold": 350
+            },
+            "filter": {
+                "rho": 20,
+                "theta": 0.15
+            }
+        },
+        "similarpoints": {
+            "range": 9
+        },
+        "correctlines": {
+            "amount": 9
+        }
+    },
+    "figuredetector": {
+        "white": {
+            "normal": {
+                "lower": [1, 2, 3],
+                "upper": [4, 5, 6]
+            },
+            "king": {
+                "lower": [1, 2, 3],
+                "upper": [4, 5, 6]
+            },
+        },
+        "black": {
+            "normal": {
+                "lower": [1, 2, 3],
+                "upper": [4, 5, 6]
+            },
+            "king": {
+                "lower": [1, 2, 3],
+                "upper": [4, 5, 6]
+            }
+        }
+    }
+}
