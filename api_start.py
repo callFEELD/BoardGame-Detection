af76f3b0aefdfb0b93a136b084532238b1ffd27c
callFEELD <callfeeld@gmail.com>
2020-04-29 21:16:53 +0200

    changed port to 80

--- api_start.py
+++ api_start.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.api.main:app", host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run("src.api.main:app", host="0.0.0.0", port=80, debug=True)
